f35039b5329965619eded98a9edac362c336c612
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-08-22 08:25:21 -0400

    adding 2014 and 2017 cluster locations

---
+++ src/prepare-shp/match-clusters-to-districts-2014.R
@@ -0,0 +1,48 @@
+################################################################################
+#' @description Match GPS coordinates of clusters to BD adm2 districts
+#' @return spreadsheet with columns for cluster number and district name
+################################################################################
+#' Clear environment
+rm(list = ls())
+#' Libraries
+library(sf)
+#' Inputs
+source("./src/util.R")
+# Location masked of DHS clusters
+ea <- st_read("./data/BD_2014_DHS_08202025_155_120781/BDGE71FL", layer = "BDGE71FL")
+# Bangladesh district boundaries
+bangladesh_2 <- st_read("./data/bgd_adm_bbs_20201113_SHP", layer = "bgd_admbnda_adm2_bbs_20201113")
+################################################################################
+
+# convert to sf object with same CRS as shapefile
+ea_crs <- st_as_sf(ea, coords = c("longitude", "latitude"), crs = st_crs(bangladesh_2))
+
+# join each point to the district polygon it falls within
+points_with_district <- st_join(ea_crs, bangladesh_2)
+
+# There are a few that were NA in the merge
+nrow(subset(points_with_district, is.na(ADM2_EN))) # 3
+# subset(points_with_district, is.na(ADM2_EN))
+# subset(ea_crs, DHSCLUST %in% c(17, 21, 544))
+# # Fill in these two manually based on nearby clusters
+# subset(points_with_district, DHSCLUST %in% c(17, 21))
+# subset(points_with_district, DHSCLUST %in% c(16, 17, 18, 19, 20, 21, 22))
+points_with_district$ADM1_EN[is.na(points_with_district$ADM2_EN) & 
+                               points_with_district$DHSCLUST %in% c(17, 21)] <- "Barisal"
+points_with_district$ADM2_EN[is.na(points_with_district$ADM2_EN) & 
+                               points_with_district$DHSCLUST %in% c(17)] <- "Barisal"
+points_with_district$ADM2_EN[is.na(points_with_district$ADM2_EN) & 
+                               points_with_district$DHSCLUST %in% c(21)] <- "Bhola"
+# Cluster 544 has 0 for lat and long. drop.
+points_with_district <- subset(points_with_district, !is.na(ADM2_EN))
+
+df <- data.frame(DHSCLUSTER = as.numeric(as.character(points_with_district$DHSCLUST)),
+                 ADM2_EN = as.character(points_with_district$ADM2_EN),
+                 ADM1_EN = as.character(points_with_district$ADM1_EN))
+nrow(subset(df, is.na(ADM2_EN))) # 0 
+nrow(subset(df, is.na(ADM1_EN))) # 0
+
+# Save --------------------------------------------------------------------
+
+write.csv(df, "./gen/prepare-shp/output/cluster-locations-2014.csv", row.names = FALSE)
+
