816c960c7354acb0dd0f68d9f6104b4150f40577
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-08-29 13:37:20 -0400

    improving aggregation to pull adm1 and amd0 number of obs and direct estimates from directly calculated files

--- src/validation/aggregate-adm2-pred.R
+++ src/validation/aggregate-adm2-pred.R
@@ -1,5 +1,5 @@
 ################################################################################
-#' @description Aggregate adm2 predictions to adm1 level for all predicted outcomes/models
+#' @description Aggregate adm2 predictions to adm0 and adm1 level
 #' @return 
 ################################################################################
 #' Clear environment
@@ -11,72 +11,180 @@ library(here)
 library(cmdstanr)
 #' Inputs
 source("./src/util.R")
-# direct adm2 estimates
+ind <- read_excel("./data/ind-info.xlsx", sheet = "indicators")
+info <- read.csv("./gen/model/audit/model-info.csv")
+# direct estimates
 direct_adm2 <- read.csv("./gen/calculate-direct/output/direct-estimates.csv")
+direct_adm1 <- read.csv("./gen/calculate-direct/output/direct-estimates-adm1.csv")
+direct_adm0 <- read.csv("./gen/calculate-direct/output/direct-estimates-adm0.csv")
 ################################################################################
 
-# outcomes
-v_outcomes <- c("nt_ch_micro_vas", "nt_ch_micro_dwm", "nt_ebf", "nt_wm_micro_iron", "rh_anc_4vs", "rh_anc_1tri")
-l_mod <- list()
-l_res <- list()
-for(i in 1:length(v_outcomes)){
+# subset to included indicators
+df_ind <- subset(ind, status == "include")
+
+# vector of indicators
+v_var <- unique(df_ind$variable)
+
+dat <- data.frame()
+for(i in 1:length(v_var)){
   
-  # select outcome
-  outcome <- v_outcomes[i]
-  # subset direct_adm2 weights this outcome (will differ whether mother-level or child-level)
-  ind_dat <- subset(direct_adm2, variable == outcome)[,c("ADM2_EN", "ADM1_EN", "sum_wgt")]
+  myoutcome <- v_var[i]
+  print(myoutcome)
   
-  # second loop for all models that have been run for this outcome
-  v_modfits <- list.files("./gen/model/fit")
-  v_modfits <- v_modfits[grepl(".rds", v_modfits)]
-  v_modfits <- v_modfits[grepl(outcome, v_modfits)]
+  # selected model for indicator
+  mymodel <- subset(df_ind, variable == myoutcome)$model
   
-  for(j in 1:length(v_modfits)){
-    
-    file_name <- v_modfits[j]
-    # read rds file that contains names of stan csv files
-    stan_csv_filenames <- readRDS(here("gen", "model", "fit", paste0(file_name)))
-    # read stan csv files as cmdstan object
-    stanfit <- as_cmdstan_fit(stan_csv_filenames)
-    
-    # extract model estimates
-    df_p = stanfit$draws(format = "df", variables=c('p'),  inc_warmup = F) |> mutate(chain=as.character(.chain)) 
-    colnames(df_p) <- colnames(df_p) %>%
-      gsub("\\[", "", .) %>%
-      gsub("\\]", "", .) %>%
-      gsub(",","_", .)
-    
-    # merge onto info for direct estimates that has adm1 and adm2 labels, and adm2 weights (sum_wgt)
-    df_p_t <- as.data.frame(t(df_p[,1:nrow(ind_dat)]))
-    names(df_p_t) <- paste0("X", 1:ncol(df_p_t))
-    df_post <- cbind(ind_dat, df_p_t)
-    
-    # Group by adm1 and calculate weighted mean for adm2 estimates
-    df_post_div <- df_post |> as.data.frame() |>
-      group_by(ADM1_EN) |>
-      summarise(across(all_of(colnames(df_post)[grepl('X',colnames(df_post))]), ~ weighted.mean(., w = sum_wgt)))
-    
-    # compute mean and credible interval across all posterior samples
-    alpha = 0.05
-    df_post_div$post_mean <- df_post_div[,2:(nrow(df_p)+1)] |> rowMeans()
-    df_quantile =  apply(df_post_div[,2:nrow(df_p)], 1 , quantile , probs = c(alpha/2,1-alpha/2) , na.rm = TRUE ) |> t()
-    df_post_div$qt_lb <- df_quantile[, 1]
-    df_post_div$qt_ub <- df_quantile[, 2]
-    df_post_div$length95 <- df_post_div$qt_ub - df_post_div$qt_lb
-    mean(df_post_div$length95)
-    df_post_div <- df_post_div |> select(-colnames(df_post)[grepl('X',colnames(df_post))])
-    
-    filecol <- sub("csv_files-fit-", "", file_name)
-    filecol <- sub(".rds", "", filecol)
-    df_post_div$file <- filecol
-    
-    l_mod[[j]] <- df_post_div
+  # Load model info for our model
+  myinfo <- subset(info, outcome == myoutcome)[,c("vers", "test", "cov")]
+  myinfo$model <- paste(myinfo$vers, myinfo$test, sep = "-")
+  
+  # # subset direct_adm2 weights this outcome
+  # mywt <- subset(direct_adm2, variable == myoutcome)[,c("ADM2_EN", "ADM1_EN", "obs_un", "obs_wn", "n_obs", "obs_un_adm1_avg", "obs_wn_adm1_avg")]
+  # # sum up adm1 and adm0 weights for merging onto results below
+  # mywt_adm1 <- mywt %>%
+  #   group_by(ADM1_EN) %>%
+  #   summarise(obs_un = sum(obs_un, na.rm = TRUE), 
+  #             obs_wn = sum(obs_wn, na.rm = TRUE), 
+  #             n_obs = sum(n_obs, na.rm = TRUE))
+  # mywt_adm0 <- mywt %>%
+  #   mutate(ADM0_EN = "Bangladesh") %>%
+  #   group_by(ADM0_EN) %>%
+  #   summarise(obs_un = sum(obs_un, na.rm = TRUE), 
+  #             obs_wn = sum(obs_wn, na.rm = TRUE), 
+  #             n_obs = sum(n_obs, na.rm = TRUE))
+  # # fill in missing weights at adm2 level with adm1 average prior to aggregation
+  # mywt_adm2 <- mywt %>%
+  #   mutate(obs_un = case_when(
+  #     is.na(obs_un) ~ obs_un_adm1_avg,
+  #     TRUE ~ obs_un
+  #   ),
+  #   obs_wn = case_when(
+  #     is.na(obs_wn) ~ obs_wn_adm1_avg,
+  #     TRUE ~ obs_wn
+  #   )) %>%
+  #   select(ADM2_EN, ADM1_EN, obs_un, obs_wn, n_obs)
+  # subset direct_adm2 weights this outcome
+  # fill in missing weights at adm2 level with adm1 average prior to aggregation
+  mywt_adm2 <- direct_adm2 %>%
+    filter(variable == myoutcome) %>%
+    mutate(obs_un = case_when(
+      obs_un == 0 ~ obs_un_adm1_avg,
+      TRUE ~ obs_un
+    ),
+    obs_wn = case_when(
+      obs_wn == 0 ~ obs_wn_adm1_avg,
+      TRUE ~ obs_wn
+    )) %>%
+    select(ADM2_EN, ADM1_EN, obs_un, obs_wn, n_obs)
+  
+  # subset adm1 and adm0 weights
+  mywt_adm1 <- direct_adm1 %>%
+    filter(variable == myoutcome) %>%
+    select(ADM1_EN, dir, dir_var, obs_un, obs_wn, n_obs)
+  mywt_adm0 <- direct_adm0 %>%
+    filter(variable == myoutcome) %>%
+    mutate(ADM0_EN = "Bangladesh") %>%
+    select(ADM0_EN, dir, dir_var, obs_un, obs_wn, n_obs)
+
+  ## VARIANCE SMOOTHING MODEL
+  
+  # model file for this indicator
+  filename_modfits <- list.files("./gen/model/fit")
+  filename_modfits <- filename_modfits[grepl(".rds", filename_modfits)]
+  filename_modfits <- filename_modfits[grepl(myoutcome, filename_modfits)]
+  filename_modfits <- filename_modfits[grepl(mymodel, filename_modfits)]
+  if(myoutcome == "nt_wm_micro_iron"){
+    filename_modfits <- filename_modfits[!grepl("nt_wm_micro_iron_any", filename_modfits)]
   }
-  df_outcomemods <- do.call(rbind, l_mod)
-  l_res[[i]] <- df_outcomemods
+  
+  # read rds file that contains names of stan csv files
+  stan_csv_filenames <- readRDS(here("gen", "model", "fit", paste0(filename_modfits)))
+  # read stan csv files as cmdstan object
+  stanfit <- as_cmdstan_fit(stan_csv_filenames)
+  
+  # extract prevalence
+  df_p = stanfit$draws(format = "df", variables=c('p'),  inc_warmup = F) |> mutate(chain=as.character(.chain)) 
+  colnames(df_p) <- colnames(df_p) %>%
+    gsub("\\[", "", .) %>%
+    gsub("\\]", "", .) %>%
+    gsub(",","_", .)
+  df_p_t <- as.data.frame(t(df_p[,1:nrow(mywt_adm2)]))
+  names(df_p_t) <- paste0("X", 1:ncol(df_p_t))
+  
+  # merge on adm2 weights
+  df_post <- cbind(mywt_adm2, df_p_t)
+  
+  # Group by adm1/adm0 and aggregate weighted mean
+  df_p_adm1 <- df_post |> as.data.frame() |>
+    group_by(ADM1_EN) |>
+    summarise(across(all_of(colnames(df_post)[grepl('X',colnames(df_post))]), ~ weighted.mean(., w = obs_wn)))
+  df_p_adm0 <- df_post |> as.data.frame() |>
+    mutate(ADM0_EN = "Bangladesh") %>%
+    group_by(ADM0_EN) %>%
+    summarise(across(all_of(colnames(df_post)[grepl('X',colnames(df_post))]), ~ weighted.mean(., w = obs_wn)))
+
+  # compute mean and credible interval across all posterior samples
+  alpha = 0.05
+  # adm1
+  df_p_adm1$post_mean <- df_p_adm1[,2:(nrow(df_p)+1)] |> rowMeans()
+  df_p_adm1$post_var <- apply(df_p_adm1[, 2:(nrow(df_p))], 1, var)
+  df_quantile =  apply(df_p_adm1[,2:nrow(df_p)], 1 , quantile , probs = c(alpha/2,1-alpha/2) , na.rm = TRUE ) |> t()
+  df_p_adm1$qt_lb <- df_quantile[, 1]
+  df_p_adm1$qt_ub <- df_quantile[, 2]
+  df_p_adm1$length95 <- df_p_adm1$qt_ub - df_p_adm1$qt_lb
+  df_p_adm1 <- df_p_adm1 |> select(-colnames(df_post)[grepl('X',colnames(df_post))])
+  df_p_adm1$admin_level <- "adm1"
+  df_p_adm1$ADM0_EN <- "Bangladesh"
+  # merge on adm1 weights
+  df_p_adm1 <- df_p_adm1 %>%
+    left_join(mywt_adm1, by = "ADM1_EN")
+  
+  # adm0
+  df_p_adm0$post_mean <- df_p_adm0[,2:(nrow(df_p)+1)] |> rowMeans()
+  df_p_adm0$post_var <- apply(df_p_adm0[, 2:(nrow(df_p))], 1, var)
+  df_quantile =  apply(df_p_adm0[,2:nrow(df_p)], 1 , quantile , probs = c(alpha/2,1-alpha/2) , na.rm = TRUE ) |> t()
+  df_p_adm0$qt_lb <- df_quantile[, 1]
+  df_p_adm0$qt_ub <- df_quantile[, 2]
+  df_p_adm0$length95 <- df_p_adm0$qt_ub - df_p_adm0$qt_lb
+  df_p_adm0 <- df_p_adm0 |> select(-colnames(df_post)[grepl('X',colnames(df_post))])
+  df_p_adm0$admin_level <- "adm0"
+  df_p_adm0$ADM1_EN <- NA
+  # merge on adm0 weights
+  df_p_adm0 <- df_p_adm0 %>%
+    left_join(mywt_adm0, by = "ADM0_EN")
+  
+  # combine adm1 and adm0
+  df_agg <- rbind(df_p_adm0, df_p_adm1)
+  df_agg <-  df_agg %>%
+    mutate(variable = myoutcome) %>%
+    mutate(model = mymodel) %>%
+    left_join(myinfo, by = "model") %>%
+    select(variable, admin_level, ADM0_EN, ADM1_EN, 
+           dir, dir_var,
+           post_mean, post_var, qt_lb, qt_ub, 
+           obs_un, obs_wn, n_obs, 
+           model, cov)
+  
+  dat <- rbind(df_agg, dat)
+  
 }
-df_res <- do.call(rbind, l_res)
+
+dat <- dat[order(dat$variable, dat$admin_level, dat$ADM0_EN, dat$ADM1_EN),]
+
+# make sure no estimates are outside of intervals
+nrow(subset(dat, post_mean > qt_ub | post_mean < qt_lb)) # 0
+
+# Note:
+# for indicator from dhs2014, Dhaka adm1 encompassed Mymensingh, which was broken out for later surveys
+# however, i took cluster locations and matched them up with most recent adm1 and adm2 shape files
+# so for me, all my estimates (even for data from old surveys) are for the present shape boundaries
+# this includes both the direct estimates i calculated as well as those that were predicted with the model
+# for the direct estimate for the indicator from 2014, the survey design was set through v023 (which wouldn't distinguish between dhaka and mymensingh),
+# however the estimates would still be output for both
+subset(df_ind, dhs_svyyr == 2014)$variable
+# so in any case... my direct adm1 estimates for this indicator won't match what's in the dhs report for dhaka and mymensingh
+# but i think it is ok to compare my aggregated modeled adm2 estimates and directly calculated adm1 estimates
 
 # Save --------------------------------------------------------------------
 
-write.csv(df_res, file = "./gen/validation/temp/agg-adm2-pred.csv", row.names = FALSE)
+write.csv(dat, file = "./gen/validation/output/pred-agg.csv", row.names = FALSE)
--- src/validation/calc-agg-error.R
+++ src/validation/calc-agg-error.R
@@ -12,34 +12,43 @@ library(sf)
 library(stringr)
 #' Inputs
 source("./src/util.R")
-# direct adm1 estimates
-direct <- read.csv("./gen/calculate-direct/audit/direct-estimates-adm1.csv")
+ind <- read_excel("./data/ind-info.xlsx", sheet = "indicators")
+# direct estimates
+direct_adm1 <- read.csv("./gen/calculate-direct/output/direct-estimates-adm1.csv")
+direct_adm0 <- read.csv("./gen/calculate-direct/output/direct-estimates-adm0.csv")
 # aggregated adm2 predictions
-agg <- read.csv("./gen/validation/temp/agg-adm2-pred.csv")
+#agg <- read.csv("./gen/validation/temp/agg-adm2-pred.csv")
+agg <- read.csv("./gen/validation/output/pred-agg.csv")
 # model info
 modinfo <- read.csv("./gen/model/audit/model-info.csv")
 # Bangladesh division boundaries
 bangladesh_1 <- st_read("./data/bgd_adm_bbs_20201113_SHP", layer = "bgd_admbnda_adm1_bbs_20201113")
 ################################################################################
 
+# subset to included indicators
+df_ind <- subset(ind, status == "include")
+
+# vector of indicators
+v_var <- unique(df_ind$variable)
+
+# adm1 --------------------------------------------------------------------
+
 # calculate 95% confidence intervals for direct, length
-direct$se <- sqrt(direct$dir_var)
-direct$lb <- direct$dir - 1.96 * direct$se
-direct$ub <- direct$dir + 1.96 * direct$se
-direct$length95 <- direct$ub - direct$lb
-direct$dhs_indicator_code <- direct$dir_var <- direct$se <- NULL
+dir_adm1 <- direct_adm1
+dir_adm1$se <- sqrt(dir_adm1$dir_var)
+dir_adm1$lb <- dir_adm1$dir - 1.96 * dir_adm1$se
+dir_adm1$ub <- dir_adm1$dir + 1.96 * dir_adm1$se
+dir_adm1$length95 <- dir_adm1$ub - dir_adm1$lb
+dir_adm1$dhs_indicator_code <- dir_adm1$dir_var <- dir_adm1$se <- NULL
 
 # create columns for aggregated
-names(agg)[which(names(agg) == "qt_lb")] <- "lb"
-names(agg)[which(names(agg) == "qt_ub")] <- "ub"
-names(agg)[which(names(agg) == "post_mean")] <- "agg"
-# merge on outcome
-agg <- merge(agg, modinfo[,c("file","outcome", "vers", "test")], by = "file")
-names(agg)[which(names(agg) == "outcome")] <- "variable"
-agg$plotlabel <- paste0(str_pad(agg$vers, width = 3, side = "left", pad = "0"), "-", agg$test)
+agg_adm1 <- subset(agg, admin_level == "adm1")
+names(agg_adm1)[which(names(agg_adm1) == "qt_lb")] <- "lb"
+names(agg_adm1)[which(names(agg_adm1) == "qt_ub")] <- "ub"
+names(agg_adm1)[which(names(agg_adm1) == "post_mean")] <- "agg"
 
 # merge
-dat <- merge(direct, agg, by = c("variable", "ADM1_EN"), suffixes = c("_dir", "_agg"))
+dat <- merge(dir_adm1, agg_adm1, by = c("variable", "ADM1_EN"), suffixes = c("_dir", "_agg"))
 
 # calculate interval overlap
 dat$int_overlap <- check_overlap(dat$lb_dir, dat$ub_dir, dat$lb_agg, dat$ub_agg)
@@ -49,55 +58,107 @@ dat$error <- dat$agg - dat$dir
 
 # Plot RMSE
 p1 <- dat %>% 
-  group_by(variable, plotlabel) %>%
+  group_by(variable) %>%
   mutate(n = n(),
          sqerror = (error)^2) %>%
   summarise(RMSE = mean(sqerror)) %>%
   ggplot() +
-  geom_bar(aes(x=plotlabel, y = RMSE), stat = "identity") +
-  facet_wrap(~variable, ncol = 1, scales = "free_y") +
+  geom_bar(aes(x=variable, y = RMSE), stat = "identity") +
   labs(x = "model", title = "Error") +
   theme_bw() +
   theme(text = element_text(size = 10)) +
   coord_flip()
-ggsave(paste0("./gen/validation/audit/error.png"), p1, width = 5.5, height = 10, units = "in", dpi = 300)
+ggsave(paste0("./gen/validation/audit/adm1-error.png"), p1, width = 5.5, height = 10, units = "in", dpi = 300)
+
 
 # Plot overlap
-v_outcomes <- c("nt_ch_micro_vas", "nt_ch_micro_dwm", "nt_ebf", "nt_wm_micro_iron", "rh_anc_4vs", "rh_anc_1tri")
-outcome1 <- v_outcomes[3]
 plotdat <- bangladesh_1 %>% 
   left_join(dat, by = "ADM1_EN")
-p2 <- ggplot() +
-  geom_sf(data = subset(plotdat, variable == outcome1), aes(fill = int_overlap)) +
-  geom_sf(data = bangladesh_1, color = "black", fill = NA) +
-  labs(title = outcome1, subtitle = "Coverage") +
-  scale_fill_manual(values = c("TRUE" = "#00BFC4", "FALSE" = "#F8766D")) +
-  theme_void() +
-  theme(
-    text = element_text(size = 10),
-    panel.background = element_rect(fill = "white", color = NA),
-    plot.background = element_rect(fill = "white", color = NA)
-  ) +
-  facet_wrap(~plotlabel, nrow = 2)
-ggsave(paste0("./gen/validation/audit/overlap-", outcome1, ".png"), p2, width = 12, height = 6)
+for(i in 1:length(v_var)){
+  myoutcome <- v_var[i]
+  p2 <- ggplot() +
+    geom_sf(data = subset(plotdat, variable == myoutcome), aes(fill = int_overlap)) +
+    geom_sf(data = bangladesh_1, color = "black", fill = NA) +
+    labs(title = myoutcome, subtitle = "Interval overlap") +
+    scale_fill_manual(values = c("TRUE" = "#00BFC4", "FALSE" = "#F8766D")) +
+    theme_void() +
+    theme(
+      text = element_text(size = 10),
+      panel.background = element_rect(fill = "white", color = NA),
+      plot.background = element_rect(fill = "white", color = NA)
+    ) 
+  ggsave(paste0("./gen/validation/audit/adm1-overlap-", myoutcome, ".png"), p2, width = 12, height = 6)
+}
 
 # Plot uncertainty intervals
-v_outcomes <- c("nt_ch_micro_vas", "nt_ch_micro_dwm", "nt_ebf", "nt_wm_micro_iron", "rh_anc_4vs", "rh_anc_1tri")
-outcome2 <- v_outcomes[3]
-direct$name <- "Direct"
-agg$name <- agg$plotlabel
-dat2 <- bind_rows(direct, agg)
+dat2 <- bind_rows(dir_adm1 %>% mutate(name = "Direct"), agg_adm1 %>% mutate(name = "agg"))
 dat2$value <- ifelse(!is.na(dat2$dir), dat2$dir, dat2$agg)
 p3 <- dat2 %>%
-  filter(variable %in% outcome2) %>%
   ggplot(aes(x = ADM1_EN, y = value, color = name, group = name)) +
   geom_errorbar(aes(ymin = lb, ymax = ub), position = position_dodge(width = 0.8), width = 0.2) +
   geom_point(position = position_dodge(width = 0.8)) +
-  labs(x = "", y = "", title = outcome2) +
+  labs(x = "", y = "") +
   theme_bw() +
   coord_flip() +
   theme(text = element_text(size = 10), legend.title=element_blank()) +
-  scale_color_discrete(guide = guide_legend(reverse = TRUE)) 
+  scale_color_discrete(guide = guide_legend(reverse = TRUE)) +
+  facet_wrap(~variable)
 
-ggsave(paste0("./gen/validation/audit/uncert-int-", outcome2,".png"), p3, width = 5, height = 5.5, units = "in", dpi = 300)
+ggsave(paste0("./gen/validation/audit/adm1-uncert-int.png"), p3, width = 12, height = 10, units = "in", dpi = 300)
+
+# adm0 --------------------------------------------------------------------
+
+# calculate 95% confidence intervals for direct, length
+dir_adm0 <- direct_adm0
+dir_adm0$se <- sqrt(dir_adm0$dir_var)
+dir_adm0$lb <- dir_adm0$dir - 1.96 * dir_adm0$se
+dir_adm0$ub <- dir_adm0$dir + 1.96 * dir_adm0$se
+dir_adm0$length95 <- dir_adm0$ub - dir_adm0$lb
+dir_adm0$dhs_indicator_code <- dir_adm0$dir_var <- dir_adm0$se <- NULL
+
+# create columns for aggregated
+agg_adm0 <- subset(agg, admin_level == "adm0")
+names(agg_adm0)[which(names(agg_adm0) == "qt_lb")] <- "lb"
+names(agg_adm0)[which(names(agg_adm0) == "qt_ub")] <- "ub"
+names(agg_adm0)[which(names(agg_adm0) == "post_mean")] <- "agg"
+
+# merge
+dat <- merge(dir_adm0, agg_adm0, by = c("variable"), suffixes = c("_dir", "_agg"))
+
+# calculate interval overlap
+dat$int_overlap <- check_overlap(dat$lb_dir, dat$ub_dir, dat$lb_agg, dat$ub_agg)
+
+# calculate error
+dat$error <- dat$agg - dat$dir
+
+# Plot RMSE
+p1 <- dat %>% 
+  group_by(variable) %>%
+  mutate(n = n(),
+         sqerror = (error)^2) %>%
+  summarise(RMSE = mean(sqerror)) %>%
+  ggplot() +
+  geom_bar(aes(x=variable, y = RMSE), stat = "identity") +
+  labs(x = "model", title = "Error") +
+  theme_bw() +
+  theme(text = element_text(size = 10)) +
+  coord_flip()
+ggsave(paste0("./gen/validation/audit/adm0-error.png"), p1, width = 5.5, height = 10, units = "in", dpi = 300)
+
+# overlap
+table(dat$int_overlap)
+
+# Plot uncertainty intervals
+dat2 <- bind_rows(dir_adm0 %>% mutate(name = "Direct"), agg_adm0 %>% mutate(name = "agg"))
+dat2$value <- ifelse(!is.na(dat2$dir), dat2$dir, dat2$agg)
+p3 <- dat2 %>%
+  ggplot(aes(x = variable, y = value, color = name, group = name)) +
+  geom_errorbar(aes(ymin = lb, ymax = ub), position = position_dodge(width = 0.8), width = 0.2) +
+  geom_point(position = position_dodge(width = 0.8)) +
+  labs(x = "", y = "") +
+  theme_bw() +
+  coord_flip() +
+  theme(text = element_text(size = 10), legend.title=element_blank()) +
+  scale_color_discrete(guide = guide_legend(reverse = TRUE))
+ggsave(paste0("./gen/validation/audit/adm0-uncert-int.png"), p3, width = 12, height = 10, units = "in", dpi = 300)
 
